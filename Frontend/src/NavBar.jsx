9724bf6351d10756d291ce7fe95e76a51bcd1e71
Iris <zhangsi1@myvuw.ac.nz>
2026-02-07 18:24:59 +1300

    Complete the homepage, implement file uploads and establish front-end and back-end connectivity.

---
+++ Frontend/src/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+function NavBar(){
+
+return (
+    
+    <div className='NavBar'>
+    <p>AI-Agent-Benchmarking-Dashboard</p>
+    <div className="NavBar-menu"><a href="#Home">Home</a></div>
+    </div>
+)
+
+}
+
+export default NavBar;
--- Frontend/src/api/users.js
+++ Frontend/src/api/users.js
@@ -1,7 +1,7 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; 
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; 
 
-// create a new file ".env" in the folder "Frontend", put your backend url in file ".env" like .env.example
-export function fetchUsers() {
-  return fetch(`${API_BASE_URL}/users`)
-    .then(res => res.json());
-}
+// // create a new file ".env" in the folder "Frontend", put your backend url in file ".env" like .env.example
+// export function fetchUsers() {
+//   return fetch(`${API_BASE_URL}/users`)
+//     .then(res => res.json());
+// }
