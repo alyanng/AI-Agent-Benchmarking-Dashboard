41b9c947dd18cd88b0ddebdf3faee44aa7d322a5
alyan <gierraalya@myvuw.ac.nz>
2026-02-15 15:10:59 +1300

    Retreived error data. Calculated stdev of each configid. Added test graphs page in router to access and check it

--- Backend/get_ai_data.py
+++ Backend/get_ai_data.py
@@ -8,3 +8,7 @@ async def get_configurations(project_id: int):
     configurations = get_config_results(project_id)
     return configurations
 
+@router.get("/get_stability_data") 
+async def get_stability(configuration_id:int):
+    stability = get_stability(configuration_id)
+    return stability
