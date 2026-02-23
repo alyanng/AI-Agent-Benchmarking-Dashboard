773f5405a4eb359ebc5027e2148c9fad9dde2da1
camelliabi <camelliabi0316@gmail.com>
2026-02-24 10:17:54 +1300

    fix: Remove unused useNavigate import in add_system_prompt.jsx

--- Frontend/src/addSystemPromptPages/add_system_prompt.jsx
+++ Frontend/src/addSystemPromptPages/add_system_prompt.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import NavBar from "../NavBar";
 
 function AddSystemPrompt(){
