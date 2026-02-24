f910c130376402c52f53fd2e50cd645446d7f65f
camelliabi <camelliabi0316@gmail.com>
2026-02-24 17:52:37 +1300

    fix: Remove unused imports in Home.jsx
    - Removed unused useEffect import
    - Removed unused navigate variable
    - Removed unused FileUploadwithtext import (already commented out in JSX)

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,21 +1,13 @@
-import { useEffect } from "react";
-//import { fetchUsers } from "./api/users"; 
-import FileUploadwithtext from "./FileUploadwithtext";
 import NavBar from "./NavBar";
 import './Home.css'
-import { useNavigate } from "react-router-dom";
 import Send_to_Mcp from "./Send_to_Mcp";
 
 function Home() {
-  const navigate = useNavigate(); 
-
   return (
     <div className="Home">
       <div className="Home-NavBar"><NavBar/> </div>
   
       <Send_to_Mcp/>
-      
-      {/* <FileUploadwithtext/> */}
     </div>
   );
 }
